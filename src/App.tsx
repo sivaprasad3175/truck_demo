7a0e079bd5afaf891fc456ed9712b0c443384047
siva <siva@sivas-MacBook-Air.local>
2025-08-09 11:28:06 +0530

    Setup GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
